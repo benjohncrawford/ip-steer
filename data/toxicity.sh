cf69d307f66545ba35d3c47c976173d080f1d4b5
Benjamin Crawford <benjohncrawford@gmail.com>
2026-06-15 15:32:38 -0700

    update toxicity script

--- data/toxicity.sh
+++ data/toxicity.sh
@@ -16,7 +16,7 @@ uv run python -u data/toxicity/preprocess_rtp.py
 uv run python -u data/toxicity/format_dataset.py
 
 # extract activations
-uv run python -u data/toxicity/extract_activations.py -m Falcon-7B-Base -l 14 -b 10
-uv run python -u data/toxicity/extract_activations.py -m Mistral-7B-Base -l 15 -b 10
-uv run python -u data/toxicity/extract_activations.py -m Llama3.1-8B-Base -l 13 -b 10
-uv run python -u data/toxicity/extract_activations.py -m Qwen2.5-7B-Base -l 13 -b 10
+uv run python -u data/toxicity/extract_activations.py -m Falcon-1B-Base -l 14 -b 10
+# uv run python -u data/toxicity/extract_activations.py -m Mistral-7B-Base -l 15 -b 10
+# uv run python -u data/toxicity/extract_activations.py -m Llama3.1-8B-Base -l 13 -b 10
+# uv run python -u data/toxicity/extract_activations.py -m Qwen2.5-7B-Base -l 13 -b 10
