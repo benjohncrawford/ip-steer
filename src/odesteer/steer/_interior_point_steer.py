6eff9aa86b06ddb91eb420029bf87b9770f4e50d
Benjamin Crawford <benjohncrawford@gmail.com>
2026-06-25 14:47:01 -0700

    fix devices

--- src/odesteer/steer/_interior_point_steer.py
+++ src/odesteer/steer/_interior_point_steer.py
@@ -58,6 +58,7 @@ class BaseIPSteer(Steer):
         return eta*torch.sum(torch.square(diff)) + torch.log(self.clf.forward(X))
 
     def solve(self, X_0: Tensor, eta_0 = 1e-6, tol = 1e-6, max_iter = 10) -> Tensor:
+        self.X_feas.to(X_0.device)
         error = 10e6
         X = self.X_feas.clone()
         eta = eta_0
