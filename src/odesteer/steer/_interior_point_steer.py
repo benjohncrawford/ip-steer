c2f5b7d06200800dca72adb36afcb6e8d676c246
Benjamin Crawford <benjohncrawford@gmail.com>
2026-07-13 14:06:32 -0700

    fix memory error

--- src/odesteer/steer/_interior_point_steer.py
+++ src/odesteer/steer/_interior_point_steer.py
@@ -147,7 +147,7 @@ class BaseIPSteer(Steer):
                 print(f"Iteration {outer_k}:\nerror: {outer_error}\nX:{X}\neta:{eta}\nobj: {y}\nh(a): {self.clf.forward(X)}\nfeasible: {self.check_feasible(X)}")
                 print("-------------------------------")
                 # Compute max change between previous and current x to see if we have converged to final solution
-                outer_prev_X = X
+                outer_prev_X = X.clone()
                 outer_k += 1
                 eta = min(eta * self.delta, max_eta)
         return X.detach()
