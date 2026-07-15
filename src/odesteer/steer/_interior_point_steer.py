e6026f562755293ff9ad84301321f8941da92b43
Benjamin Crawford <benjohncrawford@gmail.com>
2026-07-15 10:38:07 -0700

    add diagnostics

--- src/odesteer/steer/_interior_point_steer.py
+++ src/odesteer/steer/_interior_point_steer.py
@@ -137,15 +137,15 @@ class BaseIPSteer(Steer):
                     
                     # Compute max change between previous and current x to see if we have converged to central path
                     inner_error = self.calc_error(inner_prev_X, X)
-                    # print("-------------------------------")
-                    # print(f"Inner Iteration {inner_k}:\nerror: {inner_error}\nX:{X}\ninner_prev_X:{inner_prev_X}")
-                    # print("-------------------------------")
+                    print("-------------------------------")
+                    print(f"Inner Iteration {inner_k}:\nerror: {inner_error}\nX:{X}\ninner_prev_X:{inner_prev_X}")
+                    print("-------------------------------")
                     inner_prev_X = X.clone()                    
                     inner_k += 1
                 outer_error = self.calc_error(outer_prev_X, X)
-                # print("-------------------------------")
-                # print(f"Iteration {outer_k}:\nerror: {outer_error}\nX:{X}\neta:{eta}\nobj: {y}\nh(a): {self.clf.forward(X)}\nfeasible: {self.check_feasible(X)}")
-                # print("-------------------------------")
+                print("-------------------------------")
+                print(f"Iteration {outer_k}:\nerror: {outer_error}\nX:{X}\neta:{eta}\nobj: {y}\nh(a): {self.clf.forward(X)}\nfeasible: {self.check_feasible(X)}")
+                print("-------------------------------")
                 # Compute max change between previous and current x to see if we have converged to final solution
                 outer_prev_X = X.clone()
                 outer_k += 1
